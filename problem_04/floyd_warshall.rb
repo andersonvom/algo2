dc018ce73e11a776affc0520e6756e50b33e8ed4
Anderson Mesquita <andersonvom@gmail.com>
2013-01-19 01:36:16 -0500

    Added Floyd-Warshall variation to verify that it computes the shortest path

---
+++ problem_04/floyd_warshall.rb
@@ -0,0 +1,40 @@
+
+# What does this variation compute?
+def floyd_warshall_variation()
+  edges = %w( 0,1  1,2  2,3  1,3 )
+  num = edges.join.gsub(",","").split("").uniq.size
+
+  a = Array.new(num) { Array.new(num) { Array.new(num) } }
+  
+  # initialize
+  num.times do |k|
+    num.times do |i|
+      num.times do |j|
+        a[i][j][k] = Float::INFINITY
+        a[i][j][k] = 0 if i == j
+        a[i][j][k] = 1 if edges.include? "#{i},#{j}"
+      end
+    end
+  end
+
+  # run
+  (1..num-1).each do |k|
+    num.times do |i|
+      num.times do |j|
+        a[i][j][k] = [
+          a[i][j][k-1],
+          a[i][k][k-1] + a[k][j][k-1]
+        ].min
+      end
+    end
+  end
+
+  # print
+  num.times do |i|
+    num.times do |j|
+      puts "#{i},#{j}: #{a[i][j][num-1]}"
+    end
+  end
+
+end
+
