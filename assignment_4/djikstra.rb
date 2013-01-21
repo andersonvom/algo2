174c6cf6b0496449654dcc7545dd338236867f90
Anderson Mesquita <andersonvom@gmail.com>
2013-01-20 19:03:21 -0500

    Added Djikstra skeleton

---
+++ assignment_4/djikstra.rb
@@ -0,0 +1,21 @@
+require './graph.rb'
+
+class Djikstra
+  attr_accessor :graph
+
+  def initialize(a_graph)
+    self.graph = a_graph.dup
+  end
+
+  def run(source)
+    
+  end
+end
+
+if $0 == __FILE__
+  input = $1 ? File.open($1) : STDIN
+  graph = Graph.read_input(input)
+  d = Djikstra.new(graph)
+  puts d.run(1)
+end
+
