954638cd3386d45817ce5f7314b6db4b65471712
Anderson Mesquita <andersonvom@gmail.com>
2013-01-20 21:24:30 -0500

    Modified heap to keep track of indices

--- assignment_1/03.heap.rb
+++ assignment_1/03.heap.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/ruby
 
 class Heap
-  attr_accessor :tree
+  attr_accessor :tree, :indices
 
   def initialize(*ary)
     self.tree = [nil] # first element is padded w/nil to speed calculations
+    self.indices = {}
     elems = ary
     elems = ary.first if ary.count == 1
     add *elems
@@ -13,6 +14,7 @@ class Heap
   def add(*elems)
     elems.each do |elem|
       self.tree << elem
+      indices[elem] = tree.size-1
       bubble_up elem, tree.size-1
     end
     self
@@ -24,30 +26,31 @@ class Heap
 
   def delete(elem)
     return unless elem
-    removed = nil
-    self.tree.each_with_index do |e, idx|
-      if e == elem
-        removed = delete_at(idx)
-        break
-      end
-    end
-    removed
+    delete_at( indices[elem] ) if indices[elem]
   end
 
   def delete_at(idx)
-    return unless tree[idx]
-    return tree.delete_at(idx) if idx == tree.count - 1
+    return unless tree[idx] # no such element
 
-    elem = self.tree[idx]
-    last = self.tree.delete_at -1
-    self.tree[idx] = last if self.tree[idx]
+    elem = tree[idx]
+    indices.delete(elem)
+
+    # removing last element, nothing special to do
+    return tree.delete_at(idx) if idx == tree.size - 1
+
+    # removing someone in the middle
+    last = tree.delete_at -1
+    tree[idx] = last
+    indices[last] = idx
     bubble_up tree[idx], idx
     bubble_down tree[idx], idx
     elem
   end
 
   def swap(idx1, idx2, next_operation)
-    self.tree[idx1], self.tree[idx2] = [tree[idx2], tree[idx1]]
+    elem1, elem2 = tree[idx1], tree[idx2]
+    indices[elem1], indices[elem2] = idx2, idx1
+    tree[idx1], tree[idx2] = elem2, elem1
     send(next_operation, tree[idx2], idx2)
   end
 
